3757420b6bd291757fa54972b9a6e202d433f450
muhammadridwan352009-hue <muhammadridwan352009@gmail.com>
2026-07-30 09:48:57 +0700

    Init repo and start feature/ui-form

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/service/dashboard_screen.dart';
+
+void main() {
+  runApp(const SakuSiswaApp());
+}
+
+class SakuSiswaApp extends StatelessWidget {
+  const SakuSiswaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SakuSiswa',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.teal,
+      ), // ThemeData
+      home: const DashboardScreen(),
+    ); // MaterialApp
+  }
+}
+
+// DashboardScreen dipindahkan ke lib/views/dasboard_screen.dart
